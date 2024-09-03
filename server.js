89adc04fc421f9f2b46c24e4d889c46de57dd2d4
Maria Lukowich <mlukowich27@gmail.com>
2024-09-03 12:22:28 -0600

    setting up the backend repo

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+
+import "dotenv/config";
+
+const app = express();
+
+import router from "./routes/"  // add routes file
+
+const { PORT, CORS_ORIGIN } = process.env;
+
+app.use(express.json()); 
+
+app.use(express.static("public")); 
+
+app.use(cors({ origin: CORS_ORIGIN })); 
+
+app.use(router); 
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
