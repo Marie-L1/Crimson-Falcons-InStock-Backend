5e5ce7a1897a333405d5f735ed21d9dd3da8769b
Maria Lukowich <mlukowich27@gmail.com>
2024-09-04 11:50:30 -0600

    adding 404 to get single warehouse, POST/CREATE a new warehouse with status codes and validation

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -26,12 +26,52 @@ router.get("/warehouses/:id", async (req, res) => {
         // query for warehouse
         const warehouse = await req.knexDb("warehouses").where({ id }).first();
 
+        // Validation - check if the warehouse id was found
+        if(!warehouse){
+            return res.status(404).send("Warehouse id not found");
+        }
+
         res.status(200).json(warehouse)
     }catch(error){
         console.error("Error fetching warehouse", error)
         res.status(500).send("Error fetching warehouse")
     }
-})
+});
+
+// POST/CREATE a new warehouse
+router.post("/warehouses", async (req, res) =>{
+    try{
+        const {warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email} = req.body;
+
+         // Check for missing properties
+         if (!warehouse_name || !address || !city || !country || !contact_name || !contact_position || !contact_email || !contact_phone) {
+            return res.status(400).send("Error: Missing properties");
+        }
+
+        // Validation - check if the email includes "@"
+        if (!contact_email.includes("@")) {
+            return res.status(400).send("Error: Invalid email address");
+        }
+
+        // Validation - check the length of the contact phone number
+        if (contact_phone.length < 10) {
+            return res.status(400).send("Error: Invalid phone number");
+        }
+
+        // insert the new warehouse into the database and return the new warehouse
+        const result = await req.knexDb("warehouses").insert(req.body);
+
+        // fetch the new warehouse
+        const newWarehouseId = result[0]; // return the id
+        const newWarehouse = await req.knexDb("warehouses").where({ id: newWarehouseId }).first();
+
+        res.status(201).json(newWarehouse);
+
+    }catch(error){
+        console.error("Error catching warehouse", error);
+        res.status(400).send("Error creating warehouse");
+    }
+});
 
 
 export default router;
