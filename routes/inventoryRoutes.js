2a7746cd228067a98ee35a5cc4b82e5f4b87d68c
Shanemel Asuncion <shanemelasuncion@gmail.com>
2024-09-05 12:32:05 -0600

    Added search functionality

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -17,11 +17,44 @@ router.get("/inventories", async (req, res) => {
             "inventories.quantity",
         )
         res.status(200).json(inventories)
-
+        
     }catch(error){
         console.error("Error fetching the inventories", error);
     }
 })
+
+// Search route to satisfy query
+router.get("/inventories/search", async (req, res) => {
+    try {
+        const query = req.query.query; // Extract query parameter from the query string
+        if (!query) {
+            return res.status(400).json({ error: 'Query parameter "query" is required' });
+        }
+
+        const matches = await req.knexDb("inventories")
+            .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+            .select(
+                "inventories.id",
+                "warehouses.warehouse_name",
+                "inventories.item_name",
+                "inventories.description",
+                "inventories.category",
+                "inventories.status",
+                "inventories.quantity",
+            )
+            .where('warehouses.warehouse_name', 'like', `%${query}%`) // Match item_name
+            .orWhere('inventories.id', 'like', `%${query}%`)
+            .orWhere('inventories.item_name', 'like', `%${query}%`)
+            .orWhere('inventories.description', 'like', `%${query}%`)
+            .orWhere('inventories.category', 'like', `%${query}%`)
+            .orWhere('inventories.status', 'like', `%${query}%`)
+            .orWhere('inventories.quantity', 'like', `%${query}%`);
+        res.status(200).json(matches);
+    } catch (error) {
+        console.error('Error querying inventories:', error);
+        res.status(500).json({ error: 'Error querying inventories' });
+    }
+});
 
 // GET route to single inventories
 router.get("/inventories/:id", async (req, res) => {
